75e7075e81d34de2c11c805a2ca11658c4b2df34
marinaoleinik <marina.oleinik@tthk.ee>
2023-04-19 10:31:51 +0300

    mesilane lendab

--- 3.osa_Animerimine.py
+++ 3.osa_Animerimine.py
@@ -1,4 +1,4 @@
-from lib2to3 import pygram
+
 import pygame,sys
 
 pygame.init()
@@ -17,21 +17,37 @@ pygame.display.set_caption("Animatsion")
 ekraan.fill(roheline)
 kell=pygame.time.Clock()
 mesilane=pygame.image.load("bee.png")
-posX=X-mesilane.get_rect().width
-posY=Y-mesilane.get_rect().height
+#posX=X-mesilane.get_rect().width
+#posY=Y-mesilane.get_rect().height
+posX=0
+posY=0
 lõpp=False
-samm=2
+sammX=0
+sammY=0
 while not lõpp:
-	kell.tick(60)
+	kell.tick(50)
 	events=pygame.event.get()
 	for i in pygame.event.get():
 		if i.type==pygame.QUIT():
 			sys.exit()
-	ekraan.blit(mesilane,(posX,posY))
-	posX-=samm
-	if posX>X-mesilane.get_rect().width or posX<0:
-		samm=-samm
-
+	if posX==0 and posY==0:
+		sammX=1
+		sammY=0		
+	if posX==X-mesilane.get_rect().width and posY<=Y-mesilane.get_rect().height:	
+		sammX=0
+		sammY=1
+	if posX<=X-mesilane.get_rect().width and posY==Y-mesilane.get_rect().height:		
+		sammX=1
+		sammY=0
+		sammX=-sammX
+	if posX==0 and posY>=Y-mesilane.get_rect().height:					
+		sammX=0
+		sammY=1
+		sammY=-sammY
+	posX+=sammX
+	posY+=sammY
+	ekraan.blit(mesilane,(posX,posY))	
 	pygame.display.flip()
 	ekraan.fill(roheline)
+	
 pygame.quit()
