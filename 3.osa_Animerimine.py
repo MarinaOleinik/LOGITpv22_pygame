2e4ee2327f5b9fa94511d7fb571f04b307809d21
marinaoleinik <marina.oleinik@tthk.ee>
2023-04-19 12:13:16 +0300

    Funktsioonini kasutades

--- 3.osa_Animerimine.py
+++ 3.osa_Animerimine.py
@@ -2,34 +2,8 @@
 import pygame,sys
 
 pygame.init()
-#värvid
-kollane=[255,255,10]
-punane=[255,0,0]
-hall=[200,200,200]
-roosa=[255,150,255]
-roheline=[100,255,100]
-
-#ekraani suurus
-X=640
-Y=480
-ekraan=pygame.display.set_mode([X,Y])
-pygame.display.set_caption("Animatsion")
-ekraan.fill(roheline)
-kell=pygame.time.Clock()
-mesilane=pygame.image.load("bee.png")
-posX=X-mesilane.get_rect().width
-posY=Y-mesilane.get_rect().height
-#posX=0
-#posY=0
-lõpp=False
-sammX=0
-sammY=0
-while not lõpp:
-	kell.tick(50)
-	events=pygame.event.get()
-	for i in pygame.event.get():
-		if i.type==pygame.QUIT():
-			sys.exit()
+def Liikumine():
+	global posX,posY
 	if posX==0 and posY==0:
 		sammX=1
 		sammY=0		
@@ -46,8 +20,37 @@ while not lõpp:
 		sammY=-sammY
 	posX+=sammX
 	posY+=sammY
-	ekraan.blit(mesilane,(posX,posY))	
+	ekraan.blit(mesilane,(posX,posY))
 	pygame.display.flip()
+#värvid
+kollane=[255,255,10]
+punane=[255,0,0]
+hall=[200,200,200]
+roosa=[255,150,255]
+roheline=[100,255,100]
+
+#ekraani suurus
+X=800
+Y=600
+ekraan=pygame.display.set_mode([X,Y])
+pygame.display.set_caption("Animatsion")
+ekraan.fill(roheline)
+kell=pygame.time.Clock()
+mesilane=pygame.image.load("bee.png")
+posX=X-mesilane.get_rect().width
+posY=Y-mesilane.get_rect().height
+lõpp=False
+sammX=0
+sammY=0
+while not lõpp:
+	kell.tick(50)
+	events=pygame.event.get()
+	for i in pygame.event.get():
+		if i.type==pygame.QUIT():
+			sys.exit()
+	Liikumine()
+		
+	
 	ekraan.fill(roheline)
 	
 pygame.quit()
