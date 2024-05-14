a7ccc32599ce78606c6a8f4597db41788c2ac6b2
marinaoleinik <marina.oleinik@tthk.ee>
2024-05-14 11:52:26 +0300

    1.osa

--- 1.osa_akna_loomine.py
+++ 1.osa_akna_loomine.py
@@ -1,6 +1,22 @@
 import pygame
 
-
+def lill_keskel():
+    
+    lill=pygame.Rect(180,100,240,200)
+    pygame.draw.ellipse(ekraani_pind,(80,0,155),lill)
+    x=200
+    y=100
+    x_w=200
+    y_h=180
+    for i in range(12):
+        #ticks=pygame.time.get_ticks()
+        lill_väike=pygame.Rect(x,y,x_w,y_h)
+        pygame.draw.ellipse(ekraani_pind,(205,50,255),lill_väike,i+2)
+        x+=10
+        y+=10
+        x_w-=20
+        y_h-=15
+        #pygame.time.wait(200)
 pygame.init()
 kollane=[255,255,10]
 punane=[255,0,0]
@@ -12,17 +28,13 @@ pygame.display.set_caption("Esimene aken")
 
 ristkülik=pygame.Rect(0,300,640,180)
 pygame.draw.rect(ekraani_pind,(0,255,0),ristkülik)
-jalg=pygame.Rect(310,180,30,200)
-pygame.draw.rect(ekraani_pind,(50,205,0),jalg)
-
-
-lill=pygame.Rect(200,100,250,200)
-pygame.draw.ellipse(ekraani_pind,(255,255,0),lill)
-
-lill_väike=pygame.Rect(210,110,220,180)
-pygame.draw.ellipse(ekraani_pind,punane,lill_väike)
+hall=[120,120,120]
+pygame.draw.circle(ekraani_pind,hall,(500,100),50)
+pygame.draw.circle(ekraani_pind,hall,(550,150),60)
+pygame.draw.line(ekraani_pind,(22,105,44),(300,180),(300,400),20)
 
 pilt=pygame.image.load("bee.png")
+pilt=pygame.transform.scale(pilt,(70,70))
 ekraani_pind.blit(pilt,(300,50))
 
 font=pygame.font.SysFont("Broadway",40)
@@ -37,11 +49,13 @@ for i in range(30):
     pygame.draw.line(ekraani_pind,kollane,(0,0),(lopp_x,lopp_y),3)
     lopp_x-=10
     lopp_y+=12
-
-
+lill_keskel()
 pygame.display.flip()
-while True:
+while True:   
+    #lill_keskel()
     event=pygame.event.poll()
     if event.type==pygame.QUIT:
+        
         break
+    
 pygame.quit()
